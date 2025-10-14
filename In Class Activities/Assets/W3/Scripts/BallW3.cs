0a8fc7009b12c74b72442cfd611ddefa5896c53b
PeiyiXiong <pxiong2@uci.edu>
2025-10-13 19:26:23 -0700

    11

--- In Class Activities/Assets/W3/Scripts/BallW3.cs
+++ In Class Activities/Assets/W3/Scripts/BallW3.cs
@@ -1,37 +1,36 @@
 using TMPro;
 using UnityEngine;
 
-public class BallW3
+public class BallW3: MonoBehaviour
 {
     public SpriteRenderer ballRenderer;
-    private Rigidbody2D _rigidbody;
-    private float _speedMultiplier = 1.0f;
-    private float _speedThreshold = 10.0f;
+    [SerializeField] private Rigidbody2D _rigidbody;
+    [SerializeField] private float _speedMultiplier = 1.1f;
+    [SerializeField] private float _speedThreshold = 10.0f;
+    [SerializeField] private float _brightnessIncrement = 0.1f; // 每次碰撞增加亮度
 
     // ------------------------------------------------------------------------
     // This method is called by Unity whenever the ball hits something.
     private void OnCollisionEnter2D(Collision2D collision)
     {
         // STEP 1 -------------------------------------------------------------
-        // We can access the member variables of another class using the '.' symbol.
-        // For example, we can use the variabled named 'linearVelocity' on
-        //      our _rigidbody object by typing '_rigidbody.linearVelocity'.
-        //
-        // Uncomment and fix this line to multiply the _rigidbody.linearVelocity
-        //      times the _speedMultiplier variable. 
-        //
-        // This will make the ball go faster (or slower!) depending on the value
-        //      of _speedMultiplier. TUNE that value in the Inspector and see
-        //      what happens!
-
-        //rigidbody.linearvelocity *= speedmultiplier;
-
+        _rigidbody.linearVelocity *= _speedMultiplier;
         // STEP 1 -------------------------------------------------------------
 
         // STEP 9 -------------------------------------------------------------
-        // After you write the GetColorMultiplier method, simply uncomment
-        //      the below line.
-        //ballRenderer.color *= GetColorMultiplier(Mathf.Abs(_rigidbody.linearVelocity.x), Mathf.Abs(_rigidbody.linearVelocity.y));
+        // 让球颜色每次碰撞都更亮
+        Color currentColor = ballRenderer.color;
+
+        float r = currentColor.r + _brightnessIncrement;
+        float g = currentColor.g + _brightnessIncrement;
+        float b = currentColor.b + _brightnessIncrement;
+
+        // 确保 RGB 不超过 1.0
+        r = Mathf.Clamp(r, 0f, 1f);
+        g = Mathf.Clamp(g, 0f, 1f);
+        b = Mathf.Clamp(b, 0f, 1f);
+
+        ballRenderer.color = new Color(r, g, b);
         // STEP 9 -------------------------------------------------------------
     }
 
@@ -42,20 +41,17 @@ public class BallW3
     // Input: two floats - one for the X speed of the ball, one for the Y speed
     // Output: a multiplier number (with decimals)
     //
-    // Uncomment the method SIGNATURE and brackets below.
-    // Fill in the ???s with the RETURN type and 2 PARAMETERS of the method.
-    //
-    // Then, fill in the method BODY.
-    // First, calculate the average value of the speed input values.
-    // Then, IF the average speed is greater than _speedThreshold,
-    //      return a value of 1.5,
-    //      otherwise, return a value of 1.0. 
-
-    //private ??? GetColorMultiplier(??? ???, ??? ???)
-    //{
-        // write the method body here!
-        
-    //}
-
+    private float GetColorMultiplier(float xSpeed, float ySpeed)
+    {
+        float avgSpeed = (xSpeed + ySpeed) / 2.0f;
+        if (avgSpeed > _speedThreshold)
+        {
+            return 1.5f;
+        }
+        else
+        {
+            return 1.0f;
+        }
+    }
     // STEP 8 ------------------------------------------------------------------
 }
--- In Class Activities/Assets/W3/Scripts/CatW3.cs
+++ In Class Activities/Assets/W3/Scripts/CatW3.cs
@@ -14,7 +14,7 @@ public class CatW3 : MonoBehaviour
     [SerializeField] private TMP_Text _pointsText;
     [SerializeField] private TMP_Text _speechText;
     [SerializeField] private float _maxHealth = 5;
-    [SerializeField] private bool _destroyCatWhenDead;
+    [SerializeField] private bool _destroyCatWhenDead = true;
 
     private bool _facingLeft;
     private bool _isGrounded = true;
@@ -28,7 +28,6 @@ public class CatW3 : MonoBehaviour
     }
 
     // ------------------------------------------------------------------------
-    // Update is called every frame
     private void Update()
     {
         // Detect input to move player left/right
@@ -65,8 +64,6 @@ public class CatW3 : MonoBehaviour
     }
 
     // ------------------------------------------------------------------------
-    // This method is called by Unity whenever the cat hits something
-    //      whose collider's "Is Trigger" option is checked.
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag.Equals("Item"))
@@ -78,7 +75,6 @@ public class CatW3 : MonoBehaviour
     }
 
     // ------------------------------------------------------------------------
-    // This method is called by Unity whenever the cat hits something.
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag.Equals("ground"))
@@ -92,84 +88,57 @@ public class CatW3 : MonoBehaviour
             ChangeColor(ball);
 
             // STEP 2 ---------------------------------------------------------
-            // Below this comment, CALL the method named DecreaseHealth.
-            // Notice this method's return type is void- that means we don't
-            //      have to store the result anywhere.
-            
-
+            // Call DecreaseHealth method
+            DecreaseHealth();
             // STEP 2 ---------------------------------------------------------
 
             // STEP 6 ---------------------------------------------------------
-            // Write an IF STATEMENT below that does the following:
-            // IF the cat's health is below or equal to 0, AND
-            //      the _destroyCatWhenDead flag is true,
-            // then CALL the DestroyCat method.
-            //
-            // Try toggling the Destroy Cat When Dead setting on the Inspector,
-            //      and see how the cat is removed ONLY when it's checked!
+            // Destroy cat if health <= 0 and flag is true
+            if (_health <= 0 && _destroyCatWhenDead)
+            {
+                DestroyCat();
+                
+            }
             
-
             // STEP 6 ---------------------------------------------------------
         }
     }
 
     // STEP 3 -----------------------------------------------------------------
-    // This method decreases the player's health and updates the UI.
-    //
-    // Fill in the method BODY.
-    // You do NOT need to change the method signature (return or parameters).
-    //
-    // In this method:
-    // 1. Decrease the value of _health by one point.
-    // 2. Uncomment and fix the next line so that it change the value of
-    //      _healthText.text to display "health = " and the new value of _health.
-    //    '_healthText' is a TMP_Text Component, and 'text' is the variable that
-    //     decides the text that displays in the UI for this Component.
-    //      Hint: there's just one operator missing!
     private void DecreaseHealth()
     {
-        // write Step 3 below this comment!
+        // Decrease health by 1
+        _health -= 1;
 
+        // Update health text
+        _healthText.text = "health = " + _health;
 
         // STEP 5 -------------------------------------------------------------
-        // Once you've finished Step 4, CALL the GetHealthSpeechText method
-        //      and store the result in _speechText's text variable.
-        // This will look very similar to the above line to change _healthText ;)
-
-
+        // Update speech text
+        _speechText.text = GetHealthSpeechText();
         // STEP 5 -------------------------------------------------------------
     }
     // STEP 3 -----------------------------------------------------------------
 
     // STEP 4 -----------------------------------------------------------------
-    // This method decides what TEXT to display depending on how much health
-    //      the cat has left, to warn the player about low health.
-    //
-    // First, UNCOMMENT the method signature and brackets.
-    // Then, replace the ??? with the RETURN TYPE you think the method should use.
-    //
-    // Finally, fill out the method BODY. This method should:
-    // 1. IF the value of _health is less than HALF of _maxHealth,
-    //      return "OH NO!".
-    // 2. Otherwise, return "ouch".
-
-    //private ??? GetHealthSpeechText()
-    //{
-        // put the method body here!
-        
-    //}
-    
+    private string GetHealthSpeechText()
+    {
+        if (_health < _maxHealth / 2)
+        {
+            return "OH NO!";
+        }
+        else
+        {
+            return "ouch";
+        }
+    }
     // STEP 4 -----------------------------------------------------------------
 
     // ------------------------------------------------------------------------
     private void ChangeColor(BallW3 ball)
     {
         // STEP 7 -------------------------------------------------------------
-        // Set the value of the _spriteRenderer's color variable to the value
-        //      of the ball's ballRenderer's color variable.
-        // This means you'll need to use the '.' twice to get to the color :)
-
-
+        _spriteRenderer.color = ball.ballRenderer.color;
         // STEP 7 -------------------------------------------------------------
     }
     
